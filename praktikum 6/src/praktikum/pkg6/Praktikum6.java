7db92a3ac1ad6f19e71766789da06fc061749f89
Titaniaaurellia <titania_aurellia_29rpl@telkomschools.sch.id>
2021-01-14 12:42:28 +0700

    upload project

---
+++ praktikum 6/src/praktikum/pkg6/Praktikum6.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum.pkg6;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class Praktikum6 {
+    public static void main(String[] args) {
+       int sp;
+        for (sp = 0; sp < 2; sp++) {
+            System.out.println("");
+        }//spasi 
+        int zx = 4;
+        for (int z = 0; z < 5; z++) {
+            System.out.print("@");
+        }
+        System.out.println("");
+        System.out.println("   @");
+        System.out.println("  @");
+        System.out.println(" @");
+        for (int z = 0; z < 5; z++) {
+            System.out.print("@");
+        }
+        System.out.println(""); 
+    }   
+}
