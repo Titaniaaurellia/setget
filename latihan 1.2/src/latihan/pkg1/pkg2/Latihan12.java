432a3f84d2ed6784f079be66265def24cf17e6b9
Titaniaaurellia <titania_aurellia_29rpl@telkomschools.sch.id>
2021-01-14 12:01:11 +0700

    upload project

---
+++ latihan 1.2/src/latihan/pkg1/pkg2/Latihan12.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan.pkg1.pkg2;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class Latihan12 {
+
+    public static void main(String[] args) {
+        //deklarasi variabel
+        int bil = 1;
+        do{
+            //menampilkan output ke user
+            System.out.print(bil + " ");
+            bil++;
+           //deklarasi variabel
+           }while (bil<=10);
+    }
+}
