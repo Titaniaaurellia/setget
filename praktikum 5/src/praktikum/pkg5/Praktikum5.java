dd9e3c5e1a2521c4b2636535d59c779ee81ce18d
Titaniaaurellia <titania_aurellia_29rpl@telkomschools.sch.id>
2021-01-14 12:42:17 +0700

    upload project

---
+++ praktikum 5/src/praktikum/pkg5/Praktikum5.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum.pkg5;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class Praktikum5 {
+
+    public static void main(String[] args) {
+        //deklarasi variabel
+        int a = 4;
+        for (int b = 1; b <= a; b++){
+            for (int c = 1; c <= b; c++){
+                //menampilkan output
+                System.out.print("@");
+            }
+            //menampilkan output ke user
+            System.out.println();
+        }
+    }
+    
+}
