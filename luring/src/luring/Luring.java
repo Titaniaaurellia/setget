104909e144195d813b5f997bffe1cfe640f3ba4d
Titaniaaurellia <titania_aurellia_29rpl@telkomschools.sch.id>
2021-04-01 07:48:41 +0700

    upload project

---
+++ luring/src/luring/Luring.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luring;
+import java.util.Scanner;
+/**
+ *
+ * @author MOKLET-2
+ */
+public class Luring {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//membuat class baru
+        siswa s = new siswa();
+
+        System.out.println("Nama:" + s.nama);
+        System.out.println("Alamat:" + s.alamat);
+        System.out.println("No Telp : " + s.shownotelp);
+       
+}
+} 
---
+++ luring/src/luring/siswa.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package luring;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class siswa {
+
+    String nama = " Juminten ";
+    String alamat = " Merkurius blok A";
+    String shownotelp = "081234567890";
+
+    private class notelp {
+
+        String notelp = "081234567890";
+
+        public void nama() {
+            System.out.println("Juminten");
+        }
+
+        public void alamat() {
+            System.out.println("Merkurius Blok - A");
+        }
+
+       
+        public void shoenomertelpon() {
+            System.out.print("Nomer Telpon : ");
+        }
+    }
+}
